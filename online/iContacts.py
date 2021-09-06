54e3d2b9192f84367623c0824e3de0d1e1bf0714
Adahy <adahy.plas@gmail.com>
2021-09-06 13:02:42 +0200

    UPDATED

--- online/iContacts.py
+++ online/iContacts.py
@@ -1,11 +1,17 @@
 from sqlite3 import *
 from flask import *
+from hashlib import *
 
 conn = connect("../database.db")
 curs = conn.cursor()
 
 app = Flask(__name__)
 
+def hashdata(data):
+    #hash data
+    data = bytes(data, 'utf-8')
+    data = sha512(data).hexdigest()
+    return data
 @app.route("/")
 def acc():
     return "hello"
@@ -13,8 +19,12 @@ def acc():
 def connexion():
     return render_template("connexion.html")
 
-@app.route('/iContacts', methods = ['POST'])
+@app.route('/iContacts', methods = ['GET'])
 def iContacts():
-    return "hello"
-
+    resultat = request.args
+    mail = resultat["Addr"].lower()
+    mail = hashdata(mail)
+    Key = resultat["Key"]
+    Key = hashdata(Key)
+    return render_template("iContacts.html", mail=mail, key=Key)
 app.run(host="192.168.1.7", port=1313, debug=True)
